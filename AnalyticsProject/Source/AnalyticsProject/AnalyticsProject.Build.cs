be2b4007f3dcfa6dff71c2116a64240d8ffdc3da
kulichin <thedark174@gmail.com>
2020-01-19 12:47:35 +0300

    UI WIP

--- AnalyticsProject/Source/AnalyticsProject/AnalyticsProject.Build.cs
+++ AnalyticsProject/Source/AnalyticsProject/AnalyticsProject.Build.cs
@@ -6,7 +6,7 @@ public class AnalyticsProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "FirebaseAnalytics" });
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 	}
 }
